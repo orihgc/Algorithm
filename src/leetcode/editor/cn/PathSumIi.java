79d82bea52d9b240247e80d403907f9441c9e7f3
orihgc <1074464280@qq.com>
2020-07-06 02:37:03 +0800

    1

---
+++ src/leetcode/editor/cn/PathSumIi.java
@@ -0,0 +1,49 @@
+//给定一个二叉树和一个目标和，找到所有从根节点到叶子节点路径总和等于给定目标和的路径。 
+//
+// 说明: 叶子节点是指没有子节点的节点。 
+//
+// 示例: 
+//给定如下二叉树，以及目标和 sum = 22， 
+//
+//               5
+//             / \
+//            4   8
+//           /   / \
+//          11  13  4
+//         /  \    / \
+//        7    2  5   1
+// 
+//
+// 返回: 
+//
+// [
+//   [5,4,11,2],
+//   [5,8,4,5]
+//]
+// 
+// Related Topics 树 深度优先搜索
+
+package leetcode.editor.cn;
+
+public class PathSumIi{
+    public static void main(String[] args) {
+         Solution solution = new PathSumIi().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/leetcode/editor/cn/SumOfLeftLeaves.java
@@ -0,0 +1,40 @@
+//计算给定二叉树的所有左叶子之和。 
+//
+// 示例： 
+//
+// 
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+//在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24 
+//
+// 
+// Related Topics 树
+
+package leetcode.editor.cn;
+
+public class SumOfLeftLeaves{
+    public static void main(String[] args) {
+         Solution solution = new SumOfLeftLeaves().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
